886efb126e7c1c2de10e7c841b6bd3d9cd961145
Mathew Petro <petromathew@gmail.com>
2025-04-25 15:42:19 -0400

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Mathew</h1>
         <p className={styles.description}>
-          I am a Software Engineering student who excels in problem-solving, communication, and leadership, honed through diverse project work and professional experiences. Reach out if you'd like to learn more!
+          I am a Software Engineering Graduate who excels in problem-solving, communication, and leadership, honed through diverse project work and professional experiences. Reach out if you'd like to learn more!
         </p>
         <a href="mailto:petromathew@gmail.com" className={styles.contactBtn}>
           Contact Me
